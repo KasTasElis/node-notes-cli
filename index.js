1bb17217a5dc360bcd0ed53537b9a6f105b59638
EliHimself <tamosauskas09@gmail.com>
2023-10-12 10:30:34 +0300

    feat: setup for CLI

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+#!/usr/bin/env node
+
+// console.log("ARGV: ", ...process.argv);
+// console.log("ENVS: ", process.env);
+
+const note = process.argv[2];
+const newNote = {
+  id: Date.now(),
+  note,
+};
+console.log("Created new note: ", newNote);
