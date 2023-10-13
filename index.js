a21d280d62a83a540f57c4734329787b3f81e7fb
EliHimself <tamosauskas09@gmail.com>
2023-10-13 10:36:05 +0300

    feat: add additional commands

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 #!/usr/bin/env node
 
-// console.log("ARGV: ", ...process.argv);
-// console.log("ENVS: ", process.env);
-
-const note = process.argv[2];
-const newNote = {
-  id: Date.now(),
-  note,
-};
-console.log("Created new note: ", newNote);
+import "./src/commands.js";
--- src/commands.js
+++ src/commands.js
@@ -1,11 +1,59 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-yargs(hideBin(process.argv)).command(
-  "add <note>",
-  "Add a new note",
-  (yargs) => {},
-  () => {
-    console.log("Adding a new note!", yargs);
-  }
-);
+yargs(hideBin(process.argv))
+  .command(
+    "new <note>",
+    "Add a new note",
+    (yargs) => {
+      // Wtf is this for?
+      return yargs.positional("note", {
+        describe: "The content of the note you want to create",
+        type: "string",
+      });
+    },
+    (argv) => {
+      console.log("Adding a new note: ", argv.note);
+    }
+  )
+  .command(
+    "delete <noteId>",
+    "Delete a note",
+    () => {},
+    (argv) => {
+      console.log("Deleting note with id: ", argv.noteId);
+    }
+  )
+  .command(
+    "list",
+    "List all notes",
+    () => {},
+    () => {
+      console.log("Listing all notes");
+    }
+  )
+  .command(
+    "find <search>",
+    "Find a note",
+    () => {},
+    (argv) => {
+      console.log("Reading note with id: ", argv.noteId);
+    }
+  )
+  .command(
+    "update <noteId> <note>",
+    "Update a note",
+    () => {},
+    (argv) => {
+      console.log("Updating note with id: ", argv.noteId);
+    }
+  )
+  .command(
+    "reset",
+    "Reset the database",
+    () => {},
+    () => {
+      console.log("Resetting the DB!");
+    }
+  )
+  .parse();
